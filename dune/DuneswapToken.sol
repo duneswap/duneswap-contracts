19887829c95008e3f90fc03217050b6724131267
0x6a656c6c6f <97002237+duneswap@users.noreply.github.com>
2022-01-03 01:14:47 -0500

    init

---
+++ dune/DuneswapToken.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract DuneswapToken is ERC20, ERC20Permit, Pausable, AccessControl {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant RESCUER_ROLE = keccak256("RESCUER_ROLE");
+    bytes32 public constant TAKE_FEE_ROLE = keccak256("TAKE_FEE_ROLE");
+    bytes32 public constant ROUTER_ROLE = keccak256("ROUTER_ROLE");
+
+    uint256 private _maxSupply = 1000000000 * 10**decimals(); // 1 billion tokens is maximum supply
+    uint256 private _initialSupply = 100000 * 10**decimals(); // 100,000 tokens is the initial supply
+
+    event TokensRescued(address indexed sender, address indexed token, uint256 value);
+
+    constructor() ERC20("Duneswap Token", "DUNE") ERC20Permit("Duneswap Token") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(RESCUER_ROLE, _msgSender());
+        _mint(_msgSender(), _initialSupply);
+    }
+
+    function _msgSender() internal view override returns (address) {
+        return msg.sender;
+    }
+
+    /**
+     * @dev Returns the maximum amount of tokens that can be minted.
+     */
+    function maxSupply() public view returns (uint256) {
+        return _maxSupply;
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        require(totalSupply() + amount <= _maxSupply, "ERC20: cannot mint more tokens, cap exceeded");
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function rescueTokens(IERC20 token, uint256 value) external onlyRole(RESCUER_ROLE) {
+        token.transfer(_msgSender(), value);
+
+        emit TokensRescued(_msgSender(), address(token), value);
+    }
+
+}
